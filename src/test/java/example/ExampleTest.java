de4ae5bb80ad3c228049623705cb0493a0577ba7
LeoVaris <leovaris04@gmail.com>
2021-07-21 18:43:05 +0300

    initial project structure

---
+++ src/test/java/example/ExampleTest.java
@@ -0,0 +1,13 @@
+package example;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+
+}
